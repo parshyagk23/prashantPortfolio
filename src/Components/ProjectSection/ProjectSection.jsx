aa9bb77d11eae7aa3223ab1231c7a5eb33d0d595
parshyagk23 <gkprashant52@gmail.com>
2025-02-09 14:46:59 +0530

    updated

--- src/Components/ProjectSection/ProjectSection.jsx
+++ src/Components/ProjectSection/ProjectSection.jsx
@@ -11,16 +11,26 @@ import weatherimg from '../../assets/images/weather.png'
 import { GetProjectData } from '../../Api/Project';
 const ProjectSection = () => {
   const [projectData, setProjectData] = useState()
+  const [visibleIndex, setVisibleIndex] = useState(0)
   const getData = async () => {
     const res = await GetProjectData();
     setProjectData(res)
 
   }
+  const handlePrev = () => {
+    if (visibleIndex <= 0) return
+    setVisibleIndex((prevIndex) => prevIndex - 1);
+  }
+  const handleNext = () => {
+    if (visibleIndex >= projectData.length - 3) return
+    setVisibleIndex((prevIndex) => prevIndex + 1);
+  }
 
   useEffect(() => {
-    getData()
-  })
-  const ProjectImage = [quizAppImg,taskImg, swipToryImg, notesImg, calculatorimg, supperappimg,weatherimg]
+    getData();
+  }, [])
+
+  const ProjectImage = [quizAppImg, taskImg, swipToryImg, notesImg, calculatorimg, supperappimg, weatherimg]
   return (
     <section className={styles.section}>
       <h3>PROJECTS</h3>
@@ -28,9 +38,20 @@ const ProjectSection = () => {
         Explore some of my recent work and personal projects
       </p>
       <div className={styles.grid}>
-        {projectData?.map((project, index) => (
+        <div className={styles.prev} onClick={() => handlePrev()} >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M15 18l-6-6 6-6" />
+          </svg>
+
+        </div>
+        {projectData?.slice(visibleIndex, visibleIndex + 3)?.map((project, index) => (
           <ProjectCard key={index} project={project} imageUrl={ProjectImage[index]} />
         ))}
+        <div className={styles.next} onClick={() => handleNext()} >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M9 18l6-6-6-6" />
+          </svg>
+        </div>
       </div>
     </section>
   );
